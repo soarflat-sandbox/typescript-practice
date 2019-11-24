add60a1609b35c0524c3c3b61524f1c41b8ba49b
soarflat <soarflat777@gmail.com>
2019-11-25 02:06:21 +0900

    chore: noEmit を有効にし、JS ファイルを出力しないようにした。ついでにJSファイルも削除。

--- advanced-types/type-alias.js
+++
@@ -1,7 +0,0 @@
-var TypeAlias;
-(function (TypeAlias) {
-    var x = 10;
-    var y = '10';
-    // NumberOrStringはnumber型とstring型しか参照していないためエラー
-    var z = true;
-})(TypeAlias || (TypeAlias = {}));
--- interfaces/readonly-array.js
+++
@@ -1,9 +0,0 @@
-var a = [1, 2, 3, 4];
-// `ReadonlyArray<T>`型は変更処理を行う全てのメソッドが削除された`Array<T>`と同義
-// そのため、`ReadonlyArray<number>`を指定すると、配列が読み取り専用になる
-var ro = a;
-// 以下は全てエラーが発生する
-ro[0] = 12;
-ro.push(5);
-ro.length = 100;
-a = ro;
